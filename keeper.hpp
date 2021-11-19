033e226242a0ea09d201491db204457d586bf02a
oksaalina <90526845+oksaalina@users.noreply.github.com>
2021-11-19 20:18:00 +0300

    Add files via upload

---
+++ keeper.hpp
@@ -0,0 +1,32 @@
+//
+//  kepeer.hpp
+//  tp
+//
+//  Created by Алина Кузнецова on 13.11.2021.
+//
+
+#ifndef kepeer_hpp
+#define kepeer_hpp
+#include "car.hpp"
+#include "train.hpp"
+#include "plane.hpp"
+#include "element.hpp"
+
+class Keeper {
+private:
+    Element *head;
+    int _size;
+public:
+    Keeper();
+    explicit Keeper(int size);
+    ~Keeper();
+
+    void saveObject();
+    void pushObject(Carrier* obj);
+    void popObject(int index);
+    void loadObject();
+    int getSize();
+    Carrier* operator[] (int index);
+};
+
+#endif /* kepeer_hpp */
